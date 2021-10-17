6e0aeabeb5d7949fca1cc3bf2f8f595659ae0628
louysotopo <40571745+louysotopo@users.noreply.github.com>
2021-10-17 12:25:57 -0500

    Rest Api Django

---
+++ profiles_api/views.py
@@ -0,0 +1,48 @@
+#from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status #para devolver respuestas http 400 por ejemplo
+
+from profiles_api import serializers
+
+""" API View de Prueba  """
+class HelloApiView(APIView):
+    serializer_class = serializers.HelloSerializer
+    
+    def get(self, request, format=None):
+        """ Retornar la lista de caracteristicas del APIView """
+        an_apiview = [
+            'Usamos metodos HTTP como funciona (get, post, patch, put, delete)',
+            'Es similar a una vista tradicional de DJango',
+            'Nos da el mayor control sobre ñla lógica de nuestra aplicación',
+            'Esta mapeado manualmente a los URLs',
+        ]
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """ Crea un mensaje con nuestro nombre """
+        serializer = self.serializer_class(data=request.data) #contiene los datos que se requirieron
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name') #campo 1
+            message = f'Hello {name}'                    #campo 2
+            return Response({'message':message}) # siempre en diccionario
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
+    def put(self, request, pk=None):
+        """ maneja actualizar un objeto"""
+        return Response({'method': 'PUT'})
+    
+    def patch(self, request, pk=None):
+        """ maneja actualización parcial de un objeto"""
+        return Response({'method': 'PATCH'})
+    
+    def delete(self, request, pk=None):
+        """ borrar un objeto"""
+        return Response({'method': 'DELETE'})
+    
+    
+    
---
+++ readme.txt
@@ -0,0 +1,32 @@
+""" Funciona bajo un ambiente virtual de trabajo con las Versiones utilizadas """
+python 3.9.7
+django 3.2.7
+djangorestframework 3.12.4
+
+""" INICIO """
+Activar el ambiente de trabajo instalado y correr el proyecto
+ambiente> Scripts/activate.bat
+(ambiente) E:\APLICACIONES\PROYECTO_TAIS\Proyecto_Back\ambiente> cd REST_API
+(ambiente) E:\APLICACIONES\PROYECTO_TAIS\Proyecto_Back\ambiente\REST_API> python manage.py runserver
+
+""" Se puede acceder a modo de prueba a los siguientes urls: """
+http://127.0.0.1:8000/api/admin/
+http://127.0.0.1:8000/api/hello/ (aqui el minicrud de prueba)
+http://127.0.0.1:8000/api/admin/profiles_api/userprofile/
+
+
+""" Crear superusuario: """
+REST_API> python manage.py createsuperuser
+
+""" IMPORTANTE """
+Cada que se modifique models.py en cada modulo, hacer: 
+REST_API> python manage.py makemigrations [nombre del modulo]
+REST_API> python manage.py migrate
+
+ejemplo: python manage.py makemigrations profiles_api
+y luego  python manage.py migrate
+
+
+pdta:
+CURSO SEGUIDO: https://www.youtube.com/watch?v=Z8ADAr9KZYc 
+
